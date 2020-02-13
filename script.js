d5e10ae351f870ac4a7e802131b0477486aa0f89
Chiorean Paul <paul.chiorean08@gmail.com>
2020-02-13 11:46:07 +0200

    modify

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
+//Mark item from list with a line and different color with on click(), not simple click() because if i add another item doesn't work for him.
 $("ul").on("click", "li", function(){
     $(this).toggleClass("completed")
 });
 
 //Click on X to delete an item
-$("ul").on("click", "span", function(event){     
+$("ul").on("click", "span", function(event){      //same problem here, if i used just click without on, doesn't work for new items. Old was $("span").click(function(event){....
     $(this).parent().fadeOut(500,function(){
         $(this).remove();
     });
@@ -12,14 +13,14 @@ $("ul").on("click", "span", function(event){
 
 //Add another item
 $("input[type='text']").keypress(function(event){
-    if(event.which === 13){         //enter=13
+    if(event.which === 13){         //when I hit enter(enter=13) take text from imput
         var newItem = $(this).val();
-        $(this).val("");   // clean input field after i hit enter
         $("ul").append("<li><span><i class='fa fa-trash'></i></span> " + newItem + "</li>")   //create new li
+        $(this).val("");// clean input field after i hit enter
     }
 });
 
 $(".fa-plus").click(function(){
-    $("input[type='text'").fadeToggle()
+    $("input[type='text']").fadeToggle()
 
 });
